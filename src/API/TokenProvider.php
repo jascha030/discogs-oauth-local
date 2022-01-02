bd959bb194a04121c0ac75de7f56fbec5ebbc850
Jascha030 <contact@jaschavanaalst.nl>
2022-01-02 16:25:32 +0100

    🚧 Post request to access_token

--- src/API/TokenProvider.php
+++ src/API/TokenProvider.php
@@ -18,7 +18,8 @@ class TokenProvider
         private string $key,
         private string $signature
     )
-    {}
+    {
+    }
 
     /**
      * @throws GuzzleException
@@ -50,11 +51,14 @@ class TokenProvider
 
             @$server->handle('/', function (\Swoole\Http\Request $request, Response $response) use ($server) {
                 if (isset ($request->get['oauth_token'])) {
-                    file_put_contents(dirname(__FILE__, 3) . '/token.json', json_encode($request->get, JSON_THROW_ON_ERROR));
-                    echo "Discogs has successfully authorized your request.";
+                    $accessTokenResponse = $this->accessToken($request->get['oauth_token'], $request->get['oauth_verifier']);
+                    file_put_contents(dirname(__FILE__, 3) . '/token.json', $accessTokenResponse->getBody()->getContents());
 
+                    echo "Discogs has successfully authorized your request.";
                     @$response->end("Discogs has authorized your request.");
                     @$server->shutdown();
+
+                    return;
                 }
 
                 echo "Failed: Discogs could not authorize your request.";
@@ -65,6 +69,27 @@ class TokenProvider
 
             @$server->start();
         });
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function accessToken(string $oauthToken, string $oauthVerifier): ResponseInterface
+    {
+        $request = new Request('POST', 'https://api.discogs.com/oauth/access_token', [
+            'Content-Type'  => 'application/x-www-form-urlencoded',
+            'Authorization' => implode(',', [
+                'OAuth oauth_consumer_key="' . $this->key . '"',
+                'oauth_nonce="' . time() . '"',
+                'oauth_token="' . $oauthToken . '"',
+                'oauth_signature="' . $this->signature . '&"',
+                'oauth_signature_method="PLAINTEXT"',
+                'oauth_timestamp="' . time() . '"',
+                'oauth_verifier="' . $oauthVerifier . '"'
+            ])
+        ]);
+
+        return (new Client())->send($request);
     }
 
     /**
